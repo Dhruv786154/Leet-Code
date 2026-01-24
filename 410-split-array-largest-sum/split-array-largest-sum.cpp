0a422fe57b9fa38154e96e51e7beea620af40705
Dhananjay Purohit <dhruvpurohit6154@gmail.com>
2026-01-24 17:44:12 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (89.74%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -0,0 +1,53 @@
+bool isPossible(vector<int>& nums,int k, int mid)
+{
+    int countStudent = 1;
+    int pageSum = 0;
+    for(int i=0;i<nums.size();i++)
+    {
+        if(pageSum + nums[i] <= mid)
+        {
+            pageSum += nums[i];
+        }
+        
+        else{
+            countStudent++;
+            if(countStudent > k || nums[i] > mid)
+            {
+                return false;
+            }   
+            pageSum = 0;
+            pageSum = nums[i];
+        }
+    }
+    return true;
+
+}
+
+class Solution {
+public:
+    int splitArray(vector<int>& nums, int k) {
+        int s = 0;
+        int sum = 0;
+        for(int i=0;i<nums.size();i++)
+        {
+            sum = sum + nums[i];
+        }
+        int e = sum;
+        int ans = -1;
+        int mid = s+(e-s)/2;
+
+        while(s<=e)
+        {
+            if(isPossible(nums,k,mid))
+            {
+                ans = mid;
+                e = mid-1;
+            }
+            else{
+                s = mid + 1;
+            }
+            mid = s+(e-s)/2;
+        }
+        return ans;
+    }
+};
