fa3c2c63928db308ed65e638a8ca11a6fb6f59cf
Dhananjay Purohit <dhruvpurohit6154@gmail.com>
2026-02-15 02:05:01 +0530

    Time: 0 ms (100.00%) | Memory: 32.9 MB (21.14%) - LeetSync

---
+++ 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> addToArrayForm(vector<int>& num, int k) {
+        vector<int> ans;
+
+        int carry =0;
+        int i = num.size()-1;
+        while(i>=0 || carry>0 || k>0 )
+        {
+            int sum = carry;
+            if(i>=0)
+            {
+                sum = sum + num[i];
+                i--; 
+            }
+            if(k>0)
+            {
+                sum = sum + k % 10;
+                k = k/10;
+            }
+            ans.push_back(sum%10);
+            carry = sum/10;
+         
+        }
+        if(carry > 0)
+        {
+            ans.push_back(carry);
+        }   
+
+        // Reverse the ans vector
+           int s = 0;
+            int e = ans.size()-1;
+            while(s<e)
+            {
+                swap(ans[s++],ans[e--]);
+            }
+        return ans;
+    }
+};
