4326645efcdef4633be7e3e4db3eeb8f577fdd68
Dhananjay Purohit <dhruvpurohit6154@gmail.com>
2026-04-08 20:01:28 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (82.02%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans;
+        if(strs.empty())
+        {
+            return "";
+        }
+        for(int i=0;i<strs[0].size();i++)
+        {
+            for(int j=0;j<strs.size();j++)
+            {
+                if(strs[j][i] != strs[0][i] )
+                {
+                    return ans;
+                }
+            }
+            ans.push_back(strs[0][i]); 
+        }
+        return ans;
+
+    }
+};
