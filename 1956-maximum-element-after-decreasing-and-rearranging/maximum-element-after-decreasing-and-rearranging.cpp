69ffb5bd97aa9741434268f884483f00b9066646
Dhananjay Purohit <dhruvpurohit6154@gmail.com>
2026-06-28 14:04:41 +0530

    Time: 6 ms (78.25%) | Memory: 55.1 MB (69.21%) - LeetSync

---
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        // Use O(nlogn) time complexity  
+        sort(arr.begin(),arr.end());
+        arr[0] = 1;
+
+        for(int i=1;i<arr.size();i++)
+        {
+            if(abs(arr[i] - arr[i-1]) <= 1)
+            {
+                continue;
+            }
+            else{
+                arr[i] = arr[i-1] + 1;
+            }
+        }
+        return arr[arr.size()-1];
+
+    }
+};
