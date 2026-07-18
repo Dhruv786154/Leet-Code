78420df5307bd829349cf043451184f522b0da72
Dhananjay Purohit <dhruvpurohit6154@gmail.com>
2026-07-18 13:31:29 +0530

    Time: 0 ms (100.00%) | Memory: 31.5 MB (7.62%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,23 +1,46 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int start = 0;
-        int end = nums.size()-1;
-        while(start <= end)
-        {
-            int mid = start + (end - start) / 2;
-            if(nums[mid] == target)
-            {
-                return mid;  
-            }   
-            else if(nums[mid] < target)
-            {
-                start = mid + 1;
-            }
-            else{
-                end = mid - 1;
-            }
-        }         
+// Using Recursion
+int solve(vector<int>& nums,int s,int e,int target)
+{
+    // Base Case
+    if(s>e)
+    {
         return -1;
+    }
+    int mid = s + (e-s) / 2;
+    if(nums[mid] == target)
+    {
+        return mid;
+    }
+    else if(nums[mid] < target)
+    {
+        return solve(nums,mid+1,e,target);
+    }
+    else{
+        return solve(nums, s,mid-1, target);
+    }
+}
+    int search(vector<int>& nums, int target) {
+        int ans = solve(nums, 0, nums.size()-1,target);
+        return ans;
+        // int start = 0;
+        // int end = nums.size()-1;
+        // while(start <= end)
+        // {
+        //     int mid = start + (end - start) / 2;
+        //     if(nums[mid] == target)
+        //     {
+        //         return mid;  
+        //     }   
+        //     else if(nums[mid] < target)
+        //     {
+        //         start = mid + 1;
+        //     }
+        //     else{
+        //         end = mid - 1;
+        //     }
+        // }         
+        // return -1;
     }
 };
